3ef97a26e0a9e16d451066f128c3ee6c63bb8bb7
sunggyu-jeong <jskoder0109@gmail.com>
2025-12-15 09:15:14 +0900

    chore. 패키지 디렉토리를 포함하도록 하는 pnpm 구성 변경 완료, 오타수정

--- apps/contracts/test/Timelock.test.sol
+++ apps/contracts/test/Timelock.test.sol
@@ -8,22 +8,48 @@ contract TimeLockTest is Test {
     address public user = makeAddr("user");
     uint256 public constant START_BALANCE = 10 ether;
 
-    function setup() public {
+    function setUp() public {
         timeLock = new TimeLock();
         vm.deal(user, START_BALANCE);
     }
 
     function testDeposit() public {
-        vm.prank(user);
+        vm.startPrank(user);
 
         uint256 unlockTime = block.timestamp + 1 days;
         timeLock.deposit{ value: 1 ether }(unlockTime);
 
         assertEq(timeLock.balances(user), 1 ether);
         assertEq(timeLock.unlockTimes(user), unlockTime); 
+
+        vm.stopPrank();
     }
 
     function testRevertIfLocked() public {
-        
+        vm.startPrank(user);
+
+        uint256 unlockTime = block.timestamp + 1 days;
+        timeLock.deposit{ value : 1 ethers }(unlockTime);
+
+        vm.expectRevert();
+
+        timeLock.withdraw();
+        vm.stopPrank();
+    }
+
+    function testRevertIfUnLocked public {
+        vm.startPrank(user);
+
+        uint256 unlockTime = block.timestamp + 1 days;
+        timeLock.deposit{ value: 1 ether }(unlockTime);
+
+        vm.warp(block.timestamp + 1 days + 1 seconds);
+
+        timeLock.withdraw()
+
+        assertEq(address(user).balance, START_BALANCE)
+        assertEq(timeLock.balance(user), 0)
+
+        vm.stopPrank();
     }
 }
