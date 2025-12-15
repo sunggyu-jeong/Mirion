b9458dbee05e93e50eefba95d0e9e1a669d7687a
sunggyu-jeong <jskoder0109@gmail.com>
2025-12-15 09:40:44 +0900

    feat. forge-std에 TimeLock 계약 및 관련 테스트, CI 구성 및 서브모듈 추가

---
+++ apps/contracts/test/TimeLock.test.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { Test } from "forge-std/Test.sol";
+import { TimeLock } from "../src/Timelock.sol"
+
+contract TimeLockTest is Test {
+    TimeLock public timeLock;
+    address public user = makeAddr("user");
+    uint256 public constant START_BALANCE = 10 ether;
+
+    function setUp() public {
+        timeLock = new TimeLock();
+        vm.deal(user, START_BALANCE);
+    }
+
+    function testDeposit() public {
+        vm.startPrank(user);
+
+        uint256 unlockTime = block.timestamp + 1 days;
+        timeLock.deposit{ value: 1 ether }(unlockTime);
+
+        assertEq(timeLock.balances(user), 1 ether);
+        assertEq(timeLock.unlockTimes(user), unlockTime);
+        
+        vm.stopPrank();
+    }
+
+    function testRevertIfLocked() public {
+        vm.startPrank(user);
+
+        uint256 unlockTime = block.timestamp + 1 days;
+        timeLock.deposit{ value: 1 ether }(unlockTime);
+
+        vm.expectRevert();
+
+        timeLock.withdraw();
+        vm.stopPrank();
+    }
+
+    function testWithdrawIfUnlocked() public {
+        vm.startPrank(user);
+
+        uint256 unlockTime = block.timestamp + 1 days;
+        timeLock.deposit{ value: 1 ether }(unlockTime);
+
+        vm.warp(block.timestamp + 1 days + 1 seconds);
+
+        timeLock.withdraw();
+
+        assertEq(address(user).balance, START_BALANCE);
+        assertEq(timeLock.balances(user), 0);
+
+        vm.stopPrank();
+    }
+}
