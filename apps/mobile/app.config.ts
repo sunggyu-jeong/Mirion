49cf366a1e69976a5990906926c19b6695173bfc
sunggyu-jeong <jskoder0109@gmail.com>
2025-12-15 22:47:25 +0900

    feat. 모바일 설정, 라이브러리 추가

---
+++ apps/mobile/app.config.ts
@@ -0,0 +1,35 @@
+import { ConfigContext, ExpoConfig } from 'expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'LockPi',
+  slug: 'lock-pi',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  userInterfaceStyle: 'light',
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#ffffff',
+  },
+  assetBundlePatterns: ['**/*'],
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.moodlyteam.lockpi',
+  },
+  android: {
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#ffffff',
+    },
+    package: 'com.moodlyteam.lockpi',
+  },
+  web: {
+    favicon: './assets/favicon.png',
+  },
+  plugins: ['expo-router'],
+  experiments: {
+    typedRoutes: true,
+  },
+});
