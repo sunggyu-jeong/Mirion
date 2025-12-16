53f5b11b32039d7c2e00fbbdf0b0d4409156c645
sunggyu-jeong <jskoder0109@gmail.com>
2025-12-16 23:01:05 +0900

    feat. 공통 버튼 구현 완료

---
+++ apps/mobile/src/shared/ui/Button.tsx
@@ -0,0 +1,53 @@
+import { ActivityIndicator, Text, TouchableOpacity } from 'react-native';
+
+import { cn } from '@/src/shared/lib/utils';
+
+interface ButtonProps {
+  label: string;
+  onPress: () => void;
+  variant?: 'primary' | 'ghost';
+  size?: 'lg' | 'md';
+  disabled?: boolean;
+  isLoading?: boolean;
+  className?: string;
+}
+
+export const Button = ({
+  label,
+  onPress,
+  variant = 'primary',
+  size = 'lg',
+  disabled,
+  isLoading,
+  className,
+}: ButtonProps) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      disabled={disabled || isLoading}
+      activeOpacity={0.8}
+      className={cn(
+        'rounded-2xl flex-row justify-center items-center',
+        variant === 'primary' && 'bg-primary',
+        variant === 'ghost' && 'bg-transparent',
+        size === 'lg' && 'py-4',
+        size === 'md' && 'py-3',
+        disabled && 'bg-gray-200 opacity-50',
+        className,
+      )}
+    >
+      {isLoading ? (
+        <ActivityIndicator color={variant === 'ghost' ? '#000' : 'white'} />
+      ) : (
+        <Text
+          className={cn(
+            'font-bold text-center',
+            variant === 'primary' ? 'text-white text-lg' : 'text-text-500 text-base',
+          )}
+        >
+          {label}
+        </Text>
+      )}
+    </TouchableOpacity>
+  );
+};
