406601b76150e0d0d4f224204e1d9fc7a0d0a2d0
sunggyu-jeong <jskoder0109@gmail.com>
2025-12-18 14:02:55 +0900

    feat. 이더리움 가격을 가져오기를 위해 useEthPrice 커스텀 훅을 구현

--- apps/mobile/app/_layout.tsx
+++ apps/mobile/app/_layout.tsx
@@ -1,21 +1,33 @@
 import { StoreProvider } from '@/src/composition/providers/StoreProvider';
 import WagmiProvider from '@/src/composition/providers/WagmiProvider';
 import { Stack } from 'expo-router';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: true,
+      retry: 3
+    }
+  }
+})
 
 export default function RootLayout() {
   return (
-    <StoreProvider>
-      <WagmiProvider>
-        <Stack
-          screenOptions={{
-            headerShown: false,
-            contentStyle: { backgroundColor: '#ffffff' },
-          }}
-        >
-          <Stack.Screen name="index" />
-          <Stack.Screen name="(tabs)" />
-        </Stack>
-      </WagmiProvider>
-    </StoreProvider>
+    <WagmiProvider>
+      <QueryClientProvider client={queryClient}>
+        <StoreProvider>
+          <Stack
+            screenOptions={{
+              headerShown: false,
+              contentStyle: { backgroundColor: '#ffffff' },
+            }}
+          >
+            <Stack.Screen name="index" />
+            <Stack.Screen name="(tabs)" />
+          </Stack>
+        </StoreProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
   );
 }
