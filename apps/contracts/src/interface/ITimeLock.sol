301568cd3083277c2cbc49f7cbaede481688e88d
sunggyu-jeong <jskoder0109@gmail.com>
2026-01-08 11:04:30 +0900

    feat. 인터페이스 라이센스 추가

--- apps/contracts/src/interface/ITimeLock.sol
+++ apps/contracts/src/interface/ITimeLock.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
 interface ITimeLock {
   event Deposited(address indexed user, uint256 amount, uint256 unlockTime);
   event Withdraw(address indexed user, uint256 amount, uint256 fee);
