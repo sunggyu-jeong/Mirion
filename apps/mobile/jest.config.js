b853c0300224616c994db8ef0fddc29c624054f4
sunggyu-jeong <jskoder0109@gmail.com>
2026-03-18 13:57:22 +0900

    feat. 테스트 환경 추가 설정

--- apps/mobile/jest.config.js
+++ apps/mobile/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'react-native',
-  collectCoverage: true,
+  collectCoverage: false,
   coverageDirectory: 'coverage/js',
   coverageReporters: ['text', 'lcov', 'html'],
   coverageThreshold: {
@@ -15,5 +15,16 @@ module.exports = {
     'src/**/*.{ts,tsx}',
     '!src/**/*.d.ts',
     '!src/**/index.ts',
+  ],
+  moduleNameMapper: {
+    '^@app/(.*)$': '<rootDir>/src/app/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@widgets/(.*)$': '<rootDir>/src/widgets/$1',
+    '^@features/(.*)$': '<rootDir>/src/features/$1',
+    '^@entities/(.*)$': '<rootDir>/src/entities/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(react-native|@react-native|react-native-safe-area-context)/)',
   ],
 };
